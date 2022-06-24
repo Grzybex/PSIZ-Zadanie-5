1410150701127ec07c0c5f65e8a3c87499335abe
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-24 19:47:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 #include"wypiszInfo.h"
 #include "tworzenieNegatywu.h"
-
 int main() {
 
     wypiszInformacje();
     utworzNegatyw();
     printf("\n Negative picture created \n");
-
     return 0;
 }
 
